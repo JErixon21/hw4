83cd47ad1c6aa632ffc75ca38eed44bf52905a3e
Jake Erickson <jake212e@gmail.com>
2025-03-15 22:01:31 +0000

    Fixed home button issue

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,12 +1,20 @@
 class PlacesController < ApplicationController
-  # ... other actions ...
+  def index
+    @user = User.find_by({ "id" => session["user_id"] })
+    if @user != nil
+      @places = Place.all # Always returns a relation, even if empty
+    else
+      flash["notice"] = "Please log in to see places."
+      redirect_to "/login"
+    end
+  end
 
   def show
     @user = User.find_by({ "id" => session["user_id"] })
     if @user != nil
       @place = Place.find_by({ "id" => params["id"] })
       if @place != nil
-        @entries = Entry.where({ "place_id" => @place["id"], "user_id" => @user["id"] }) # Only user's entries
+        @entries = Entry.where({ "place_id" => @place["id"], "user_id" => @user["id"] })
       else
         redirect_to "/places"
       end
@@ -15,5 +23,22 @@ class PlacesController < ApplicationController
     end
   end
 
-  # ... other actions ...
+  def new
+    @place = Place.new
+  end
+
+  def create
+    @user = User.find_by({ "id" => session["user_id"] })
+    if @user != nil
+      @place = Place.new
+      @place["name"] = params["name"]
+      if @place.save
+        redirect_to "/places"
+      else
+        render "new"
+      end
+    else
+      redirect_to "/login"
+    end
+  end
 end
